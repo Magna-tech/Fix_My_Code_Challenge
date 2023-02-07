897a1bb527eea455746df2ef26309837e92acdb7
Nathan Omeri <magembenath@gmail.com>
2023-02-07 13:57:04 -0800

    Fix for square class (property added)- trial 3.6

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -5,26 +5,33 @@
 class square:
     """Class square"""
 
-    def __init__(self, width = 0, height = 0):
+    def __init__(self, width = 0):
         """Initialization"""
-        self.width = width
-        self.height = height
+        self.__width = width
+
+    @property
+    def width(self):
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.height
+        return self.width * self.width
 
     def PermiterOfMySquare(self):
         """Perimeter"""
-        return (self.width * 2) + (self.height * 2)
+        return (self.width * 4)
 
     def __str__(self):
         """Print sides"""
-        return "{}/{}".format(self.width, self.height)
+        return "{}/{}".format(self.width, self.width)
 
 
 if __name__ == "__main__":
-    s = square(width=12, height=9)
+    s = square(12)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
