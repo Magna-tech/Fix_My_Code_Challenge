d772d45709001d36f25b7a1311d7976b686fda8b
Nathan Omeri <magembenath@gmail.com>
2023-02-07 13:15:38 -0800

    Fix for the square class - trial 3

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,32 +1,28 @@
 #!/usr/bin/python3
-"""This program prints the area and perimeter of a square"""
-
 
 class square():
-    """Class square"""
-
+    
     width = 0
+    height = 0
 
+    
     def __init__(self, *args, **kwargs):
-        """Initialization"""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
     def PermiterOfMySquare(self):
-        """Perimeter of the square"""
-        return (self.width * 4)
+        return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        """Print side of the square"""
-        return "{}/{}".format(self.width, self.width)
-
+        return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
-    s = square(width=12)
+
+    s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
