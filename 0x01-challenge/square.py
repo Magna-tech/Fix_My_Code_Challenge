49eaef996e971df4255c01f56cb1d5e5171017c6
Nathan Omeri <magembenath@gmail.com>
2023-02-07 13:50:55 -0800

    Fix for square class - trial 3.5

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -2,12 +2,10 @@
 """Class square"""
 
 
-class square():
+class square:
     """Class square"""
-    width = 0
-    height = 0
 
-    def __init__(self, width, height):
+    def __init__(self, width = 0, height = 0):
         """Initialization"""
         self.width = width
         self.height = height
